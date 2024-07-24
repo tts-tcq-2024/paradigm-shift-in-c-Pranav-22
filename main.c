07d2f4b7226eb3f6c763238dde34ef509cc6520e
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-07-24 12:03:24 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "bms_parameters_check.h"
+
+int main()
+{
+  assert(batteryIsOk(25, 70, 0.7));
+  assert(!batteryIsOk(50, 85, 0));
+  assert(!batteryIsOk(100,20,1));
+  assert(!batteryIsOk(0,15,0.9));
+  
+}
