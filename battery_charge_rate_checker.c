2ddbf454c84b4c8148c461a8ab5c3464ddecc304
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-07-31 09:41:36 +0530

    Update battery_charge_rate_checker.c

--- battery_charge_rate_checker.c
+++ battery_charge_rate_checker.c
@@ -1,21 +1,40 @@
 #include "common_util.h"
 #include "battery_charge_rate_checker.h"
 
-battery_status battery_soc_check(float value,float min_range,float max_range)
+battery_status check_battery_chargerate_warning(float value,float range)
 {
-    float range_arr[4] = {0};
-    int tolerance = 5;
 
-    range_arr[min_tolerance] = min_range + calculate_tolerance(min_range,tolerance) ;
-    range_arr[min] = min_range;
-    range_arr[max_tolerance] = max_range - calculate_tolerance(max_range,tolerance);
-    range_arr[max] = max_range;
-
-     if(check_battery_warning(value,range_arr)== warning)
-     {
-         return check_battery_error(value,range_arr);
-     }
-
-     return normal;
+    if(value > range)
+    {
+        return warning;
+    }
+    return normal;
 }
 
+battery_status check_battery_chargerate_error(float value,float range)
+{
+
+    if(value > range)
+    {
+        return error;
+    }
+    return warning;
+}
+
+battery_status battery_charge_rate(float value,float range)
+{
+    float min_tole;
+    int tolerance = 5;
+    battery_status result1 = normal;
+    battery_status result2 = normal;
+
+    min_tole = range - calculate_tolerance(range,tolerance);
+
+    if(check_battery_chargerate_warning(value,min_tole) == warning)
+    {
+          return check_battery_chargerate_error(value,range);
+    }
+
+    return normal;
+
+}
