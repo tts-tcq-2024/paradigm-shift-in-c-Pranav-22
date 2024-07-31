e73244d6f13f8ff04d78d78d41b5df19050b1f06
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-07-31 09:10:01 +0530

    Create battery_temperature_checker.c

---
+++ battery_temperature_checker.c
@@ -0,0 +1,24 @@
+#include "common_util.h"
+#include "battery_temperature_checker.h"
+
+battery_status battery_temperature_check(float value,float min_range,float max_range)
+{
+    float min_tol;
+    float max_tol;
+    battery_status result1;
+    battery_status result2;
+    float range_arr[4] = {0};
+    int tolerance = 5;
+
+    range_arr[min_tolerance] = min_range + calculate_tolerance(min_range,tolerance) ;
+    range_arr[min] = min_range;
+    range_arr[max_tolerance] = max_range - calculate_tolerance(max_range,tolerance);
+    range_arr[max] = max_range;
+
+     if(check_battery_warning(value,range_arr)== warning)
+     {
+         return check_battery_error(value,range_arr);
+     }
+
+     return normal;
+}
